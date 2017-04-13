1b25d20e6ddbc6b94b07ef7b8998ccb438f8d4aa
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-04-13 08:58:01 -0700

    solved plus one prob

--- js/main.js
+++ js/main.js
@@ -8,3 +8,33 @@ Example
 Input: [1,2,3]      	=>	Output: [1,2,4]
 Input: [1, 9]		=>	Output: [2,0]
 */
+
+function addOne(arr) {
+  var i = arr.length-1;
+  
+  var helper = function(arr, i) {
+    if(arr[i] < 9) {
+      arr[i] = arr[i]+1;
+      return arr;
+    }
+    else if(arr[i] === 9 && arr[i-1] < 9) {
+      arr[i] = 0;
+      arr[i-1] = arr[i-1] +1;
+    }
+    else {
+      arr[i] = 0;
+      if(i === 0 ) {
+        arr.unshift(1);
+        return arr;
+      }
+      helper(arr, i-1);
+    }
+  };
+  helper(arr, i);
+  return arr;
+}
+
+console.log('PlusOne : ', addOne([9,9,9]))
+
+
+//=====================================================================================================
