b62a1064b2eca6d377dff875690a8070c9b70768
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-04-13 08:57:28 -0700

    plus one prob

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/*
+Plus One
+Given a positive integer represented by an array of digits, add one that number.
+
+Input: Array of integers
+Output: Array of integers
+Example
+Input: [1,2,3]      	=>	Output: [1,2,4]
+Input: [1, 9]		=>	Output: [2,0]
+*/
