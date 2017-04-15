6c4bc0c4997e87e8fa12fd8c7b795d5bf08d1ee4
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-04-15 09:00:08 -0700

    nums of ones

--- js/main.js
+++ js/main.js
@@ -82,3 +82,18 @@ function sortColors(arr) {
 console.log('sortColors: ', sortColors([2,1,0,0,1,2]));
  
 //====================================================================================================
+/*
+Number of Ones
+Given a sorted bit array (values of 0, and 1) determine the number of 1’s in the array.
+
+Input: Array of elements with values belong to the set S : { 0, 1 }
+Output: Integer
+Example
+Input: [0, 0, 0, 1, 1, 1]	=>	Output: 3
+Input: [0, 0, 0, 0]		=>	Output: 0
+Constraints
+Time Complexity: O(logN)
+Auxiliary Space Complexity: O(1)
+
+A linear search is not acceptable for runtime.
+*/
