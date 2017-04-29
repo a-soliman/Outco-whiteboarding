39adeecf4d1d8f518bb6d97550a10cba3a57d0b3
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-04-29 09:20:34 -0700

    spiral

--- js/main.js
+++ js/main.js
@@ -748,3 +748,24 @@ function removeDublicates(arr) {
 console.log('removeDublicates : ' + removeDublicates([1,2,2,2,3,9,9]))
 //===============================================================================================================================
 
+
+/*
+Matrix Spiral
+Given an (MxN) matrix of integers, return an array in spiral order.
+
+Input: Array of integers
+Output: Array of integers
+Example
+Input: [[1,2,3],		=>	Output: [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        [4,5,6],
+        [7,8,9]]      	
+Input: [[]]		=>	Output: []
+Constraints
+Time Complexity: O(MN)
+Auxiliary Space Complexity: O(MN)
+
+
+Values of the array will be digits 0-9.
+*/
+
+
