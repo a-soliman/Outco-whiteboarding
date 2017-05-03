c45866913822d68deda60c992fe0cc603ca8beb0
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-05-03 09:26:49 -0700

    Shortest Route

--- js/main.js
+++ js/main.js
@@ -1016,4 +1016,33 @@ function boolMat(matrix) {
 console.log('BooleanMatrix : ' + boolMat(BooleanMatrix))
 
 //===============================================================================================================================
+/*
+Shortest Route
+Given an unweighted, undirected graph which represents a metro map as follows 
+vertices are stations
+edges are the path between two stations
 
+
+Given a start station and ending station, determine the minimum number of stops that must be made to get to the destination.
+
+Input: A Graph (unweighted/undirected), a starting Vertex, and an ending Vertex
+Output: Integer
+Example
+Input: The graph represented below, Vertex A, Vertex F
+
+
+
+
+
+
+Output: 2 Stops (From A stop at C, and then stop at F)
+Constraints
+Time Complexity: O(V + E) where V is the number of Vertices and E is the number of Edges
+Auxiliary Space Complexity: O(V)
+
+A graph Vertex instance has the following properties:
+value : a string
+	edges : a list which contains references to other vertices in the Graph
+The graph has a list of all the vertices: Graph.vertices
+The Vertex values are all unique
+*/
